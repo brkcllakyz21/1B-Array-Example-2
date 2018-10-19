853652061813975f93f2fbbc57e0f8545eca3cf7
brkcllakyz21 <37548000+brkcllakyz21@users.noreply.github.com>
2018-10-20 01:20:33 +0300

    Add files via upload

---
+++ 1B Array Example 2/1B Array Example 2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1B_Array_Example_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dizi = new int[10];
+            int sayı = 0, sayaç = 0;
+
+            Console.WriteLine("Bir sayı giriniz.");
+            sayı = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < dizi.Length; i++)
+            {
+                if (sayı > dizi[i])
+                {
+                    sayaç++;
+                }
+            }
+
+            Console.WriteLine(sayı + " sayısı " + sayaç + " tane elemandan büyüktür.");
+
+
+
+        }
+    }
+}
